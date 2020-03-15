d08dc55738806d86ee02c8c7d88e229794164df3
Pilad <piladt@gmail.com>
2020-03-15 20:27:56 +0600

    Update GT_Proxy.java

--- src/main/java/gregtech/common/GT_Proxy.java
+++ src/main/java/gregtech/common/GT_Proxy.java
@@ -215,6 +215,7 @@ public abstract class GT_Proxy implements IGT_Mod, IGuiHandler, IFuelHandler {
     public boolean gt6Cable = true;
     public boolean ic2EnergySourceCompat = true;
     public boolean costlyCableConnection = false;
+    public boolean mEasyEnderIOMaterialsRecipe = false;	
     public boolean mMoreComplicatedChemicalRecipes = false;
     
     public GT_Proxy() {
