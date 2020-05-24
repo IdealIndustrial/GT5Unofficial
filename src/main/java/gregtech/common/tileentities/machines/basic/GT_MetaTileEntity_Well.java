d2dad7d6c81c19aa44413c9160c368084201b6e2
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2020-05-24 19:11:24 +0300

    fix well render

--- src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_Well.java
+++ src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_Well.java
@@ -126,11 +126,13 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     @Override
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        if(aSide == 0 && aActive) {
+        if(aSide == 1 && aActive) {
             if(fluid == null)
                 return mTextures[5][aColorIndex + 1];
-            mTextures[4][aColorIndex + 1][0] = new GT_RenderedTexture(new GT_IconContainer(fluid.getFluid().getStillIcon(), null, null));
-            return mTextures[4][aColorIndex + 1];
+            ITexture[] ar = new ITexture[2];
+            ar[1] = mTextures[4][aColorIndex+1][1];
+            ar[0] = new GT_RenderedTexture(new GT_IconContainer(fluid.getFluid().getStillIcon(), null, null));
+            return ar;
         }
         return mTextures[aSide == 0 ? 7 : aSide == 1 ?  aActive ? 4 : 5 : aSide > 1 && aSide < 6 ?  3 : 4][aColorIndex + 1];
 
