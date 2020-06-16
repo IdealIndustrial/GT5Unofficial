00a33fb090fea306d9ca1976c786de21cf5f9787
Pilad <piladt@gmail.com>
2020-06-16 17:35:13 +0600

    Update GT_MetaTileEntity_LargeTurbine_Plasma.java

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_LargeTurbine_Plasma.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_LargeTurbine_Plasma.java
@@ -38,13 +38,12 @@ public class GT_MetaTileEntity_LargeTurbine_Plasma extends GT_MetaTileEntity_Lar
                 "Controller Block for the Large Plasma Generator",
                 "Size(WxHxD): 3x3x4 (Hollow), Controller (Front centered)",
                 "1x Plasma Input Hatch (Side centered)",
-                "1x Output Hatch (Bottom centered)",
+                "Optional Output Hatch for Gas/Molten Metals (Side centered)",
                 "1x Maintenance Hatch (Side centered)",
                 "1x Dynamo Hatch (Back centered)",
                 "Tungstensteel Turbine Casings for the rest (24 at least!)",
                 "Needs a Turbine Item (Inside controller GUI)",
-                "Energy Output depending on Rotor: 6553-332595EU/t",
-                "Can output Gas or Molten Metal"};
+                "Energy Output depending on Rotor: 6553-332595EU/t"};
     }
 
     public int getFuelValue(FluidStack aLiquid) {
