d64e6953de63751b9b8c97f59f81080c18913646
Pilad <piladt@gmail.com>
2020-10-28 23:29:07 +0600

    Update GT_MetaGenerated_Item_01.java

--- src/main/java/gregtech/common/items/GT_MetaGenerated_Item_01.java
+++ src/main/java/gregtech/common/items/GT_MetaGenerated_Item_01.java
@@ -73,7 +73,7 @@ public class GT_MetaGenerated_Item_01 extends GT_MetaGenerated_Item_X32 {
 
         ItemList.Tool_DataReader.set(addItem(tLastID = 18, "Data Reader", "Now you'll know what data sticks contain"));
         addItemBehavior(32000+tLastID, new Behaviour_DataReader());
-        setElectricStats(32000+tLastID, 100000, GT_Values.V[1], 1, -3, false);
+        setElectricStats(32000+tLastID, 400000, GT_Values.V[2], 2, -3, false);
 
         GT_ModHandler.addShapelessCraftingRecipe(ItemList.Coin_Chocolate.get(1L, new Object[0]), new Object[]{OrePrefixes.dust.get(Materials.Cocoa), OrePrefixes.dust.get(Materials.Milk), OrePrefixes.dust.get(Materials.Sugar), OrePrefixes.foil.get(Materials.Gold)});
 
