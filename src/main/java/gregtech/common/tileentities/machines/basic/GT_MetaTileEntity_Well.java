33355f74c248049e7bc99360db018ef8ebdffc20
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2021-01-05 16:02:20 +0300

    reformat code and probably fix well

--- src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_Well.java
+++ src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_Well.java
@@ -1,37 +1,20 @@
 package gregtech.common.tileentities.machines.basic;
 
-import cpw.mods.fml.common.eventhandler.Event;
 import gregtech.api.enums.ConfigCategories;
-import gregtech.api.enums.Dyes;
-import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Textures;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
-import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicHull;
-import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicMachine;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicTank;
-import gregtech.api.net.GT_Packet_Block_Event;
 import gregtech.api.net.GT_Packet_ExtendedBlockEvent;
 import gregtech.api.objects.GT_IconContainer;
 import gregtech.api.objects.GT_RenderedTexture;
-import gregtech.api.objects.GT_SidedTexture;
-import gregtech.api.objects.GT_UO_Fluid;
 import gregtech.api.util.GT_Config;
 import gregtech.api.util.GT_Utility;
 import gregtech.common.GT_UndergroundOil;
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.MathHelper;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
-import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -50,9 +33,9 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     public GT_MetaTileEntity_Well(int aID, String aName, String aNameRegional, int aTier) {
         super(aID, aName, aNameRegional, aTier, 0, new String[]{
-                "Allows the extraction of deep fluids",
-                "Filling rate depends on the volume of the field",
-                "Squeeze out a liquid in a bucket or a single cell"
+                        "Allows the extraction of deep fluids",
+                        "Filling rate depends on the volume of the field",
+                        "Squeeze out a liquid in a bucket or a single cell"
                 },
                 new GT_RenderedTexture(new Textures.BlockIcons.CustomIcon("basicmachines/well/OVERLAY_SIDE_ACTIVE")),
                 new GT_RenderedTexture(new Textures.BlockIcons.CustomIcon("basicmachines/well/OVERLAY_SIDE")),
@@ -82,19 +65,19 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     @Override
     public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
-        if((fluid!=null&&fluid.amount>=1000)||aBaseMetaTileEntity.isClientSide() || !aBaseMetaTileEntity.isAllowedToWork())
+        if ((fluid != null && fluid.amount >= 1000) || aBaseMetaTileEntity.isClientSide() || !aBaseMetaTileEntity.isAllowedToWork())
             return;
         mProgresstime++;
-        if(mProgresstime>=60){
+        if (mProgresstime >= 60) {
             mProgresstime = 0;
-            FluidStack fs = GT_UndergroundOil.undergroundOil(getBaseMetaTileEntity(),1f);
+            FluidStack fs = GT_UndergroundOil.undergroundOil(getBaseMetaTileEntity(), 1f);
             fs.amount *= mEfficiency;
-            if(isFluidAllowed(fs.getFluid())) {
-                if(fluid == null)
+            if (isFluidAllowed(fs.getFluid())) {
+                if (fluid == null)
                     fluid = fs;
                 else
                     fluid.amount += fs.amount;
-                if(fluid.amount>=1000) {
+                if (fluid.amount >= 1000) {
                     aBaseMetaTileEntity.setActive(true);
                     fluid.amount = 1000;
                     NW.sendPacketToAllPlayersInRange(getBaseMetaTileEntity().getWorld(), new GT_Packet_ExtendedBlockEvent(getBaseMetaTileEntity().getXCoord(), (short) getBaseMetaTileEntity().getYCoord(), getBaseMetaTileEntity().getZCoord(), 129, fluid.getFluidID()), getBaseMetaTileEntity().getXCoord(), getBaseMetaTileEntity().getZCoord());
@@ -104,9 +87,9 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     }
 
-    protected boolean isFluidAllowed(Fluid aFluid){
-        for(Fluid tFluid : allowedFluids){
-            if(aFluid != null && tFluid != null && aFluid == tFluid)
+    protected boolean isFluidAllowed(Fluid aFluid) {
+        for (Fluid tFluid : allowedFluids) {
+            if (tFluid != null && aFluid == tFluid)
                 return true;
         }
         return false;
@@ -115,10 +98,10 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
     @Override
     public void onConfigLoad(GT_Config aConfig) {
         super.onConfigLoad(aConfig);
-        String s =  aConfig.get(ConfigCategories.machineconfig,"well.allowedFluids","water|oil");
-        mEfficiency = aConfig.get(ConfigCategories.machineconfig,"well.efficiency",1.0f);
+        String s = aConfig.get(ConfigCategories.machineconfig, "well.allowedFluids", "water|oil");
+        mEfficiency = aConfig.get(ConfigCategories.machineconfig, "well.efficiency", 1.0f);
         String[] fs = s.split("\\|");
-        for(String str : fs){
+        for (String str : fs) {
             allowedFluids.add(FluidRegistry.getFluid(str));
         }
 
@@ -126,28 +109,28 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     @Override
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        if(aSide == 1 && aActive) {
-            if(fluid == null)
+        if (aSide == 1 && aActive) {
+            if (fluid == null)
                 return mTextures[5][aColorIndex + 1];
             ITexture[] ar = new ITexture[2];
-            ar[1] = mTextures[4][aColorIndex+1][1];
+            ar[1] = mTextures[4][aColorIndex + 1][1];
             ar[0] = new GT_RenderedTexture(new GT_IconContainer(fluid.getFluid().getStillIcon(), null, null));
             return ar;
         }
-        return mTextures[aSide == 0 ? 7 : aSide == 1 ?  aActive ? 4 : 5 : aSide > 1 && aSide < 6 ?  3 : 4][aColorIndex + 1];
+        return mTextures[aSide == 0 ? 7 : aSide == 1 ? aActive ? 4 : 5 : aSide > 1 && aSide < 6 ? 3 : 4][aColorIndex + 1];
 
     }
-
 
 
     @Override
     public void receiveExtendedBlockEvent(int aID, int aValue) {
-        if(aID == 129)
-            fluid = new FluidStack(FluidRegistry.getFluid(aValue),10);
-        else if(aID == 130)
+        if (aID == 129)
+            fluid = new FluidStack(FluidRegistry.getFluid(aValue), 10);
+        else if (aID == 130)
             fluid = null;
+        getBaseMetaTileEntity().rebakeMap();
     }
-    
+
     @Override
     public ITexture[][][] getTextureSet(ITexture[] aTextures) {
         ITexture[][][] rTextures = new ITexture[14][17][];
@@ -156,8 +139,8 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
         for (int i = 0; i < aTextures.length; i++)
             if (aTextures[i] != null) for (byte c = -1; c < 16; c++) {
                 if (rTextures[i][c + 1] == null) {
-                    if(i == 4)
-                        rTextures[i][c + 1] = new ITexture[]{ new GT_RenderedTexture(new Textures.BlockIcons.CustomIcon("basicmachines/well/DEFAULT_FLUID")), aTextures[i]}; //null for fluid texture
+                    if (i == 4)
+                        rTextures[i][c + 1] = new ITexture[]{new GT_RenderedTexture(new Textures.BlockIcons.CustomIcon("basicmachines/well/DEFAULT_FLUID")), aTextures[i]}; //null for fluid texture
                     else
                         rTextures[i][c + 1] = new ITexture[]{aTextures[i]};
                 }
@@ -182,18 +165,18 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
     @Override
     public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
-        if(getBaseMetaTileEntity().isClientSide()) {
+        if (getBaseMetaTileEntity().isClientSide()) {
             return true;
         }
-        if(fluid == null)
-            GT_Utility.sendChatToPlayer(aPlayer,"Currently is empty");
+        if (fluid == null)
+            GT_Utility.sendChatToPlayer(aPlayer, "Currently is empty");
         else if (fluid.amount < 1000)
-            GT_Utility.sendChatToPlayer(aPlayer,"Currently is "+ fluid.amount/10 +"% ready");
-        if(fluid == null||fluid.amount<1000)
+            GT_Utility.sendChatToPlayer(aPlayer, "Currently is " + fluid.amount / 10 + "% ready");
+        if (fluid == null || fluid.amount < 1000)
             return false;
         ItemStack tStack = aPlayer.getHeldItem();
-        ItemStack aStack = GT_Utility.fillFluidContainer(fluid,tStack, false, true);
-        if(aPlayer.getHeldItem().stackSize==1&&aStack!=null) {
+        ItemStack aStack = GT_Utility.fillFluidContainer(fluid, tStack, false, true);
+        if (aPlayer.getHeldItem() != null && aPlayer.getHeldItem().stackSize == 1 && aStack != null) {
             aPlayer.setCurrentItemOrArmor(0, aStack);
             fluid = null;
             getBaseMetaTileEntity().setActive(false);
@@ -201,12 +184,13 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
 
             return true;
         }
+        GT_Utility.sendChatToPlayer(aPlayer, "Currently is ready");
         return false;
     }
 
     @Override
     public void saveNBTData(NBTTagCompound aNBT) {
-        aNBT.setTag("fFluid",fluid.writeToNBT(new NBTTagCompound()));
+        aNBT.setTag("fFluid", fluid.writeToNBT(new NBTTagCompound()));
         super.saveNBTData(aNBT);
     }
 
@@ -215,7 +199,6 @@ public class GT_MetaTileEntity_Well extends GT_MetaTileEntity_BasicTank {
         super.loadNBTData(aNBT);
         fluid = FluidStack.loadFluidStackFromNBT(aNBT.getCompoundTag("fFluid"));
     }
-
 
 
     @Override
