10458bb0de32ef8a678bee4338153ec176276ee3
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2021-01-07 01:22:24 +0300

    add hooklib

---
+++ src/main/java/gloomyfolken/hooklib/minecraft/HookLoader.java
@@ -0,0 +1,81 @@
+package gloomyfolken.hooklib.minecraft;
+
+import cpw.mods.fml.common.asm.transformers.DeobfuscationTransformer;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import gloomyfolken.hooklib.asm.AsmHook;
+import gloomyfolken.hooklib.asm.ClassMetadataReader;
+import gloomyfolken.hooklib.asm.HookClassTransformer;
+
+import java.util.Map;
+
+/**
+ * Удобная базовая реализация IFMLLoadingPlugin для использования HookLib.
+ * Регистрировать хуки и контейнеры нужно в registerHooks().
+ */
+public abstract class HookLoader implements IFMLLoadingPlugin {
+
+    private static DeobfuscationTransformer deobfuscationTransformer;
+
+    private static ClassMetadataReader deobfuscationMetadataReader;
+
+    static {
+        deobfuscationMetadataReader = new DeobfuscationMetadataReader();
+    }
+
+    public static HookClassTransformer getTransformer() {
+        return PrimaryClassTransformer.instance.registeredSecondTransformer ?
+                MinecraftClassTransformer.instance : PrimaryClassTransformer.instance;
+    }
+
+    /**
+     * Регистрирует вручную созданный хук
+     */
+    public static void registerHook(AsmHook hook) {
+        getTransformer().registerHook(hook);
+    }
+
+    /**
+     * Деобфусцирует класс с хуками и регистрирует хуки из него
+     */
+    public static void registerHookContainer(String className) {
+        getTransformer().registerHookContainer(className);
+    }
+
+    public static ClassMetadataReader getDeobfuscationMetadataReader() {
+        return deobfuscationMetadataReader;
+    }
+	
+	static DeobfuscationTransformer getDeobfuscationTransformer() {
+        if (HookLibPlugin.getObfuscated() && deobfuscationTransformer == null) {
+            deobfuscationTransformer = new DeobfuscationTransformer();
+        }
+        return deobfuscationTransformer;
+    }
+
+    // 1.7.x only
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        registerHooks();
+    }
+
+    protected abstract void registerHooks();
+}
