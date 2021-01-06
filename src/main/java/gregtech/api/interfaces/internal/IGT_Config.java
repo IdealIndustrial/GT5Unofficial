4496823e42d1ada5ce5b6228e656b9889f500967
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2021-01-07 01:25:39 +0300

    add annotation configs

--- src/main/java/gregtech/api/interfaces/internal/IGT_Config.java
+++ src/main/java/gregtech/api/interfaces/internal/IGT_Config.java
@@ -10,9 +10,11 @@ import java.lang.reflect.Modifier;
 
 public interface IGT_Config {
 
+
+    //todo: move all impl stuff to abstract class
     /**
      * loads everything, gets invoked via reflection from {@link gregtech.loaders.preload.GT_LoaderConfig}
-     * @param configuration is cfg instance created from file with name {@link #getName()}
+     * @param configuration is cfg instance that is created from file with name {@link #getName()}
      */
     default void load(Configuration configuration) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         loadAll(getClass(), configuration);
@@ -25,24 +27,39 @@ public interface IGT_Config {
 
     /**
      * should not be overridden!
-     * simple method to load all public static fields from config file according to their names
-     * initial values is used as default value
+     * simple method to load all static fields from config file that are annotated with {@link gregtech.api.interfaces.internal.GT_Config}
+     * initial values are used as default value ( or may be overridden)
+     * use protected fields to load from config and then process in {@link #load(Configuration)} for non common objects for eg. Map<>
      */
     default void loadAll(Class<? extends IGT_Config> loadAll, Configuration configuration) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        for (Field field : loadAll.getFields()) {
+        for (Field field : loadAll.getDeclaredFields()) {
             if (!Modifier.isStatic(field.getModifiers()))
                 continue;
-            field.set(null, getWithType(field.getType(), getProperty(field, configuration)));
+            if (!field.isAnnotationPresent(GT_Config.class))
+                continue;
+            field.setAccessible(true);
+            GT_Config config = field.getAnnotation(GT_Config.class);
+            String category = config.category();
+            String name = config.configName().equals("") ? field.getName() : config.configName();
+            String comment = config.configComment();
+            field.set(null, getWithType(field.getType(), getProperty(category, name, comment, field, configuration)));
         }
     }
     /*gregtechproxy.allowDisableToolTips = tMainConfig.get("general", "AllowDisablingLargeTooltips", true).getBoolean(true);*/
 
-    static Property getProperty(Field field, Configuration configuration) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        if (String.class.equals(field.getType())) {
+    static Property getProperty(String category, String name, String comment, Field field, Configuration configuration) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        /*if (String.class.equals(field.getType())) {
             return configuration.get("main", field.getName(), (String) field.getType().cast(field.get(null)));
+        }*/
+        if (comment.equals("")) {
+            return (Property) Configuration.class.getMethod("get", String.class, String.class, field.getType())
+                    .invoke(configuration, category, name, field.get(null));
         }
-        Method get = Configuration.class.getMethod("get", String.class, String.class, field.getType());
-        return (Property) get.invoke(configuration, "main", field.getName(), field.get(null));
+        else {
+            return (Property) Configuration.class.getMethod("get", String.class, String.class, field.getType(), String.class)
+                    .invoke(configuration, category, name, field.get(null), comment);
+
+        }
        // throw new IllegalStateException("type of "+field.getName() + ": " + field.getType().getName()+ " handling is not implemented yet");
     }
 
