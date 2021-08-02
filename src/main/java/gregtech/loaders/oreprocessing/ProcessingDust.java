310a78c1017753da616a5064cb4420b5ff27dd26
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2021-08-03 02:23:08 +0300

    replace 8000L of oxygen with 7000L in YtVaCu smelting

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingDust.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingDust.java
@@ -40,7 +40,7 @@ public class ProcessingDust implements gregtech.api.interfaces.IOreRecipeRegistr
                 if ((null != (tDustStack = GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L))) && (!aMaterial.contains(SubTag.NO_SMELTING))) {
                     if (aMaterial.mBlastFurnaceRequired) {
                         GT_ModHandler.removeFurnaceSmelting(aStack);
-                        FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(8000L) : null;
+                        FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(7000L) : null;
                         GT_Values.RA.addBlastRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), null, tFluid, null, aMaterial.mBlastFurnaceTemp > 1750 ? GT_OreDictUnificator.get(OrePrefixes.ingotHot, aMaterial.mSmeltInto, tDustStack, 1L) : GT_Utility.copyAmount(1L, new Object[]{tDustStack}), null, (int) Math.max(aMaterial.getMass() / 40L, 1L) * aMaterial.mBlastFurnaceTemp, 120, aMaterial.mBlastFurnaceTemp);
                         if (aMaterial.mBlastFurnaceTemp <= 1000) {
                             GT_ModHandler.addRCBlastFurnaceRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_Utility.copyAmount(1L, new Object[]{tDustStack}), aMaterial.mBlastFurnaceTemp);
@@ -242,7 +242,7 @@ public class ProcessingDust implements gregtech.api.interfaces.IOreRecipeRegistr
                     }
                 }
                 if (aMaterial.mBlastFurnaceRequired) {
-                    FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(8000L) : null;
+                    FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(7000L) : null;
                     GT_Values.RA.addBlastRecipe(GT_Utility.copyAmount(4L, new Object[]{aStack}), null, tFluid, null, aMaterial.mBlastFurnaceTemp > 1750 ? GT_OreDictUnificator.get(OrePrefixes.ingotHot, aMaterial.mSmeltInto, GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), 1L) : GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), null, (int) Math.max(aMaterial.getMass() / 40L, 1L) * aMaterial.mBlastFurnaceTemp, 120, aMaterial.mBlastFurnaceTemp);
                 } else {
                     gregtech.api.util.GT_ModHandler.addAlloySmelterRecipe(GT_Utility.copyAmount(4L, new Object[]{aStack}), ItemList.Shape_Mold_Ingot.get(0L, new Object[0]), GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), 130, 3, true);
@@ -258,7 +258,7 @@ public class ProcessingDust implements gregtech.api.interfaces.IOreRecipeRegistr
                 }
                 if (!aMaterial.contains(gregtech.api.enums.SubTag.NO_SMELTING)) {
                     if (aMaterial.mBlastFurnaceRequired) {
-                        FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(8000L) : null;
+                        FluidStack tFluid = aMaterial == Materials.YttriumBariumCuprate ? Materials.Oxygen.getGas(7000L) : null;
                         GT_Values.RA.addBlastRecipe(GT_Utility.copyAmount(9L, new Object[]{aStack}), null, tFluid, null, aMaterial.mBlastFurnaceTemp > 1750 ? GT_OreDictUnificator.get(OrePrefixes.ingotHot, aMaterial.mSmeltInto, GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), 1L) : GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), null, (int) Math.max(aMaterial.getMass() / 40L, 1L) * aMaterial.mBlastFurnaceTemp, 120, aMaterial.mBlastFurnaceTemp);
                         GT_ModHandler.removeFurnaceSmelting(aStack);
                     } else {
