7bcf3a6379f1e548e2bf90ef839cecacf0d3ca31
AndreySolodovnikov <37311176+AndreySolodovnikov@users.noreply.github.com>
2021-08-19 01:34:48 +0300

    balance tungsten recipes

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader2.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader2.java
@@ -22,24 +22,24 @@ public class GT_MachineRecipeLoader2 implements Runnable{
     }
 
     private void addTungstenProcessing() {
-        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.Scheelite, 1), null, Materials.HydrochloricAcid.getFluid(2000L),
-                null, get(OrePrefixes.dust, Materials.CalciumChloride, 1), get(OrePrefixes.dust, Materials.TungstigAcid, 1),
+        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.Scheelite, 6), null, Materials.HydrochloricAcid.getFluid(4000L),
+                null, get(OrePrefixes.dust, Materials.CalciumChloride, 3), get(OrePrefixes.dust, Materials.TungstigAcid, 7),
                 5 * 20, 1920);
-        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.Tungstate, 1), null, Materials.HydrochloricAcid.getFluid(2000L),
-                null, get(OrePrefixes.dust, Materials.LithiumChloride, 2), get(OrePrefixes.dust, Materials.TungstigAcid, 1),
+        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.Tungstate, 7), null, Materials.HydrochloricAcid.getFluid(4000L),
+                null, get(OrePrefixes.dust, Materials.LithiumChloride, 4), get(OrePrefixes.dust, Materials.TungstigAcid, 7),
                 5 * 20, 1920);
-        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.TungstigAcid, 1), GT_Utility.getIntegratedCircuit(1), null,
-                Materials.Water.getFluid(1000L), get(OrePrefixes.dust, Materials.TungstenTrioxide, 1), null,
+        RA.addChemicalRecipe(get(OrePrefixes.dust, Materials.TungstigAcid, 7), GT_Utility.getIntegratedCircuit(1), null,
+                Materials.Water.getFluid(3000L), get(OrePrefixes.dust, Materials.TungstenTrioxide, 4), null,
                 10*20, 90);
 
-        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 1), get(OrePrefixes.dust, Materials.Iron, 2), null,
-                null, get(OrePrefixes.ingotHot, Materials.Tungsten, 1), get(OrePrefixes.dust, Materials.BandedIron, 1),
+        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 4), get(OrePrefixes.dust, Materials.Iron, 2), null,
+                null, get(OrePrefixes.ingotHot, Materials.Tungsten, 1), get(OrePrefixes.dust, Materials.BandedIron, 5),
                 600*20, 120, 3000);
-        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 1), null, Materials.Hydrogen.getGas(3000L),
-                GT_ModHandler.getSteam(3000), get(OrePrefixes.ingotHot, Materials.Tungsten, 1),null,
+        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 4), null, Materials.Hydrogen.getGas(6000L),
+                GT_ModHandler.getSteam(9000), get(OrePrefixes.ingotHot, Materials.Tungsten, 1),null,
                 600*20, 120, 3000);
-        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 2), get(OrePrefixes.dust, Materials.Carbon, 3), null,
-                Materials.CarbonDioxide.getGas(3000), get(OrePrefixes.ingotHot, Materials.Tungsten, 2),null,
+        RA.addBlastRecipe(get(OrePrefixes.dust, Materials.TungstenTrioxide, 8), get(OrePrefixes.dust, Materials.Carbon, 6), null,
+                Materials.CarbonDioxide.getGas(9000), get(OrePrefixes.ingotHot, Materials.Tungsten, 2),null,
                 1200*20, 120, 3000);
 
     }
