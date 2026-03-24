4e8afc0e6348b09f655d7ce2c93613268217dfe8
Egor Markin <eg0rmaffin@gmail.com>
2026-03-24 22:34:52 +0300

    fix liquid output

--- src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_BasicMachine.java
+++ src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_BasicMachine.java
@@ -261,7 +261,9 @@ public abstract class GT_MetaTileEntity_BasicMachine extends GT_MetaTileEntity_B
 
     @Override
     public boolean isLiquidInput(byte aSide) {
-        return aSide != mMainFacing && (mAllowFluidInputFromOutputSide || aSide != getBaseMetaTileEntity().getFrontFacing());
+        return aSide != mMainFacing
+                && (mAllowFluidInputFromOutputSide || aSide != getBaseMetaTileEntity().getFluidFacing())
+                && (mAllowInputFromOutputSide || aSide != getBaseMetaTileEntity().getFrontFacing());
     }
 
     @Override
