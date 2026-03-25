794630b3289d364ba59c70af333d37f66c3749d5
Pilad <piladt@gmail.com>
2026-03-25 19:03:15 +0500

    Update GT_Renderer_Entity_Arrow.java (#326)
    Иногда при сборке от градла прилетали варнинги. Это фиксит их.

--- src/main/java/gregtech/common/render/GT_Renderer_Entity_Arrow.java
+++ src/main/java/gregtech/common/render/GT_Renderer_Entity_Arrow.java
@@ -3,6 +3,7 @@ package gregtech.common.render;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.client.renderer.entity.RenderArrow;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.util.ResourceLocation;
 
 public class GT_Renderer_Entity_Arrow extends RenderArrow {
@@ -14,7 +15,7 @@ public class GT_Renderer_Entity_Arrow extends RenderArrow {
         RenderingRegistry.registerEntityRenderingHandler(aArrowClass, this);
     }
 
-    protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
+    protected ResourceLocation getEntityTexture(EntityArrow entity) {
         return this.mTexture;
     }
 }
